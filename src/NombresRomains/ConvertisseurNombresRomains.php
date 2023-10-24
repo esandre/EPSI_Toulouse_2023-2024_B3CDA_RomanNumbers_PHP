968ad44d3ed2480e596d7a9a5f58534d3d2d91d4
Enzo Sandré <atelier@enzosandre.fr>
2023-10-24 16:08:53 +0200

    1 -> I

---
+++ src/NombresRomains/ConvertisseurNombresRomains.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NombresRomains;
+
+class ConvertisseurNombresRomains
+{
+    public static function Convertir(int $chiffreArabe)
+    {
+        return "I";
+    }
+}
--- test/RomanNumbersTest.php
+++ test/RomanNumbersTest.php
@@ -1,10 +1,18 @@
 <?php
 
+use NombresRomains\ConvertisseurNombresRomains;
 use PHPUnit\Framework\TestCase;
 
 final class RomanNumbersTest extends TestCase
 {
-    public function testVide(){
-        $this->assertTrue(true);
+    public function testUn() {
+        // ETANT DONNE le chiffre 1
+        $chiffreArabe = 1;
+
+        // QUAND on le convertit en nombres romains
+        $nombreRomain = ConvertisseurNombresRomains::Convertir($chiffreArabe);
+
+        // ALORS on obtient "I"
+        $this->assertEquals('I', $nombreRomain);
     }
 }
