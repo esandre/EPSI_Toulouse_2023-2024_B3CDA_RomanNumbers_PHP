ed7f722eff9da72f11c3bf1ab016c32304b4a85a
Enzo Sandré <atelier@enzosandre.fr>
2023-10-24 16:13:52 +0200

    Refacto des tests

--- test/RomanNumbersTest.php
+++ test/RomanNumbersTest.php
@@ -5,25 +5,20 @@ use PHPUnit\Framework\TestCase;
 
 final class RomanNumbersTest extends TestCase
 {
-    public function testUn() {
-        // ETANT DONNE le chiffre 1
-        $chiffreArabe = 1;
-
+    /**
+     * @dataProvider providerTestUnité
+     */
+    public function testUnité(int $chiffreArabe) {
+        // ETANT DONNE le chiffre <chiffreArabe>
         // QUAND on le convertit en nombres romains
         $nombreRomain = ConvertisseurNombresRomains::Convertir($chiffreArabe);
 
-        // ALORS on obtient "I"
-        $this->assertEquals('I', $nombreRomain);
+        // ALORS on obtient "I" répété <chiffreArabe> fois
+        $attendu = str_repeat('I', $chiffreArabe);
+        $this->assertEquals($attendu , $nombreRomain);
     }
 
-    public function testDeux() {
-        // ETANT DONNE le chiffre 2
-        $chiffreArabe = 2;
-
-        // QUAND on le convertit en nombres romains
-        $nombreRomain = ConvertisseurNombresRomains::Convertir($chiffreArabe);
-
-        // ALORS on obtient "II"
-        $this->assertEquals('II', $nombreRomain);
+    public static function providerTestUnité() {
+        return [[1], [2]];
     }
 }
