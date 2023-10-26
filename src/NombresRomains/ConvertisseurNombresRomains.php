a11c6772af1c6c465b8c531b13a324e974aee14a
Enzo Sandré <atelier@enzosandre.fr>
2023-10-26 12:00:45 +0200

    Refacto

--- src/NombresRomains/ConvertisseurNombresRomains.php
+++ src/NombresRomains/ConvertisseurNombresRomains.php
@@ -6,6 +6,10 @@ class ConvertisseurNombresRomains
 {
     public static function Convertir(int $chiffreArabe): string
     {
-        return $chiffreArabe == 1 ? "I" : ($chiffreArabe == 2 ? "II" : "III");
+        return match ($chiffreArabe) {
+            1 => "I",
+            2 => "II",
+            default => "III",
+        };
     }
 }
