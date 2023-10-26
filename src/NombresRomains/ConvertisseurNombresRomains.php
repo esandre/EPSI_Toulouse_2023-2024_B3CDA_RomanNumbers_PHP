0a0fc115464a683d113e429b20ca1e6778e443c7
Enzo Sandré <atelier@enzosandre.fr>
2023-10-26 12:06:44 +0200

    Fix

--- src/NombresRomains/ConvertisseurNombresRomains.php
+++ src/NombresRomains/ConvertisseurNombresRomains.php
@@ -10,7 +10,7 @@ class ConvertisseurNombresRomains
             1 => "I",
             2 => "II",
             3 => "III",
-            default => "IIII"
+            default => "IV"
         };
     }
 }
